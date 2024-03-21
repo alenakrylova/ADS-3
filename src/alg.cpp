efd47821a5281fd130f9069e9d4c9103de5d0845
alena krylova <150517805+alenakrylova@users.noreply.github.com>
2024-03-21 20:44:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int leftbound = 0;
+  int rightbound = size - 1;
+  int counter = 0;
+  while (leftbound <= rightbound){
+    int center = (leftbound + rightbound) / 2;
+    if (arr[center] == value){
+      counter++;
+      int temp = center - 1;
+      while (temp >= leftbound && arr[temp] == value){
+        counter++;
+        temp--;
+      }
+      temp = center + 1;
+      while (temp <= rightbound && arr[temp] == value){
+        counter++;
+        temp++; 
+      }
+      return counter;
+    }else if (arr[center] < value){
+      leftbound = center +1;
+    }else{
+      rightbound = center - 1;
+    }
+  }
   return 0; // если ничего не найдено
 }
